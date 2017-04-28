da20e35ff23e0826c9ef7fd8a6372a5f55d271dc
Thomas Ward <teward@users.noreply.github.com>
2017-04-27 20:06:49 -0400

    Refactor to use a Post object.
    Gets rid of the PostData class, which we don't need anymore.

--- apigetpost.py
+++ apigetpost.py
@@ -6,51 +6,6 @@ import time
 import html
 
 
-class PostData:
-    def __init__(self):
-        self.post_id = None
-        self.post_url = None
-        self.post_type = None
-        self.site = None
-        self.owner_url = None
-        self.owner_name = None
-        self.owner_rep = None
-        self.title = None
-        self.body = None
-        self.score = None
-        self.up_vote_count = None
-        self.down_vote_count = None
-        self.question_id = None
-
-    @property
-    def as_dict(self):
-        # Basically, return this to the dict-style response that Post(api_data=DATA) expects, for proper parsing.
-        dictdata = {
-            'title': self.title,
-            'body': self.body,
-            'owner': {'display_name': self.owner_name, 'link': self.owner_url, 'reputation': self.owner_rep},
-            'site': self.site,
-            'question_id': self.post_id,
-            'link': self.post_url,
-            'score': self.score,
-            'up_vote_count': self.up_vote_count,
-            'down_vote_count': self.down_vote_count,
-        }
-        # noinspection PyBroadException
-        try:
-            dictdata['IsAnswer'] = getattr(self, 'IsAnswer')
-        except:
-            dictdata['IsAnswer'] = False  # Assume it's not an answer
-
-        return dictdata
-
-    def __setitem__(self, key, value):
-        setattr(self, key, value)
-
-    def __getitem__(self, item):
-        getattr(self, item)
-
-
 def api_get_post(post_url):
     GlobalVars.api_request_lock.acquire()
 
@@ -80,25 +35,5 @@ def api_get_post(post_url):
         return False
     GlobalVars.api_request_lock.release()
 
-    item = response['items'][0]
-    post_data = PostData()
-    post_data.post_id = post_id
-    post_data.post_url = parsing.url_to_shortlink(item['link'])
-    post_data.post_type = post_type
-    post_data.title = html.unescape(item['title'])
-    if 'owner' in item and 'link' in item['owner']:
-        post_data.owner_name = html.unescape(item['owner']['display_name'])
-        post_data.owner_url = item['owner']['link']
-        post_data.owner_rep = item['owner']['reputation']
-    else:
-        post_data.owner_name = ""
-        post_data.owner_url = ""
-        post_data.owner_rep = 1
-    post_data.site = site
-    post_data.body = item['body']
-    post_data.score = item['score']
-    post_data.up_vote_count = item['up_vote_count']
-    post_data.down_vote_count = item['down_vote_count']
-    if post_type == "answer":
-        post_data.question_id = item['question_id']
-    return post_data
+    post_item = Post(api_response=response['items'][0])
+    return post_item
